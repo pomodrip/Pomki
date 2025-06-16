85a48c0fb94d9a3a6dacfd8d43ee8a8136c9a4d6
정지훈 <90081875+mudokja@users.noreply.github.com>
2025-06-16 16:52:32 +0900

    feat: Swagger jwt 인증 설정 추가 fix: Swagger 충돌 해결

--- backend/src/main/java/com/cooltomato/pomki/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/cooltomato/pomki/global/config/SwaggerConfig.java
@@ -1,19 +1,37 @@
 package com.cooltomato.pomki.global.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Pomki API",
+                description = "Pomki 프로젝트 API 명세서",
+                version = "v1"
+        )
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("Bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Pomki API")
-                        .description("Pomki 프로젝트 API 명세서")
-                        .version("v1.0.0")
-                );
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
     }
 } 
