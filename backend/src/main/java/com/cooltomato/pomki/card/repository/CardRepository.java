a623e38270902fa4345d15b3d714ce0b8ac3f633
davJ-star <jshsy0130@gmail.com>
2025-06-20 17:43:57 +0900

    fix: remove extraneous character in CardRepository interface

--- backend/src/main/java/com/cooltomato/pomki/card/repository/CardRepository.java
+++ backend/src/main/java/com/cooltomato/pomki/card/repository/CardRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+/
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     
