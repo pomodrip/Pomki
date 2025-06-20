c03200195c86c95ee4f9c5b7f27dd1c98229f4ff
davJ-star <jshsy0130@gmail.com>
2025-06-20 14:33:40 +0900

    refactor: comment out unused Button import in DashboardPage

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import Card from '../../components/ui/Card';
-import Button from '../../components/ui/Button';
+// import Button from '../../components/ui/Button';
 import ProgressBar from '../../components/ui/ProgressBar';
 import { useResponsive } from '../../hooks/useResponsive';
 import { useLocation } from 'react-router-dom';
