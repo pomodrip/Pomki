9a12020b5fc337d1883ee0232aac9a14bf53d238
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-06-29 20:24:54 +0900

    fix: TagRepository 누락

--- backend/src/main/java/com/cooltomato/pomki/tag/repository/TagRepository.java
+++ backend/src/main/java/com/cooltomato/pomki/tag/repository/TagRepository.java
@@ -1,8 +1,16 @@
 package com.cooltomato.pomki.tag.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cooltomato.pomki.tag.entity.Tag;
+import com.cooltomato.pomki.tag.entity.TagId;
+
 @Repository
-public class TagRepository {
+public interface TagRepository extends JpaRepository<Tag, TagId> {
+
+  List<Tag> findByMemberId(Long memberId);
     
 }
