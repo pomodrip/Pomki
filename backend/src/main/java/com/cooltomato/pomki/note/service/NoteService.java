9ac3b65ecafbb47923a4a2d662a6c35040f0192c
정지훈 <90081875+mudokja@users.noreply.github.com>
2025-07-02 17:05:23 +0900

    fix 잘못된 import 제거

--- backend/src/main/java/com/cooltomato/pomki/note/service/NoteService.java
+++ backend/src/main/java/com/cooltomato/pomki/note/service/NoteService.java
@@ -23,8 +23,6 @@ import com.cooltomato.pomki.ai.service.AIService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
-import org.checkerframework.checker.units.qual.s;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
