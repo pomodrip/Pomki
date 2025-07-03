21a14efb199764748f0e229ef3e536d072bca17a
That Egg Picker <yeomyeongforbusiness@gmail.com>
2025-07-03 09:41:12 +0900

    fix: 잘못된 import 삭제

--- backend/src/main/java/com/cooltomato/pomki/note/service/NoteService.java
+++ backend/src/main/java/com/cooltomato/pomki/note/service/NoteService.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
-import org.checkerframework.checker.units.qual.s;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
