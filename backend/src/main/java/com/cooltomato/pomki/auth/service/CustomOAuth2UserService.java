a0bb637b141a8138c3efac32c0b427f54deb041a
정지훈 <90081875+mudokja@users.noreply.github.com>
2025-07-17 00:42:21 +0900

    feat: 회원탈퇴시 소셜로그인 연동 해제

--- backend/src/main/java/com/cooltomato/pomki/auth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/cooltomato/pomki/auth/service/CustomOAuth2UserService.java
@@ -1,10 +1,13 @@
 package com.cooltomato.pomki.auth.service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,18 +24,23 @@ import com.cooltomato.pomki.member.entity.Member;
 import com.cooltomato.pomki.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final MemberRepository memberRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2AccessToken accessToken = userRequest.getAccessToken();
 
         OAuth2UserInfo userInfo = getOAuth2UserInfo(registrationId, oAuth2User);
 
@@ -40,6 +48,16 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String providerUserId = userInfo.getProviderId();
         String email = userInfo.getEmail();
         String name = userInfo.getName();
+
+        String redisKeyAccessToken = "social:accessToken:" + provider.name() + ":" + providerUserId;
+        
+        Instant expiresAt = accessToken.getExpiresAt();
+        if (expiresAt != null) {
+            Duration duration = Duration.between(Instant.now(), expiresAt);
+            redisTemplate.opsForValue().set(redisKeyAccessToken, accessToken.getTokenValue(), duration.toSeconds(), TimeUnit.SECONDS);
+        } else {
+            redisTemplate.opsForValue().set(redisKeyAccessToken, accessToken.getTokenValue(), 3, TimeUnit.HOURS);
+        }
 
         Optional<Member> member = memberRepository.findByProviderAndProviderUserId(provider, providerUserId);
         if(member.isEmpty()){
--- backend/src/main/java/com/cooltomato/pomki/global/config/RedisConfig.java
+++ backend/src/main/java/com/cooltomato/pomki/global/config/RedisConfig.java
@@ -13,6 +13,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -59,6 +60,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
