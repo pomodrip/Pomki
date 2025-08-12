9e9e615c84c14c92aab7a5e8e44dfa4234257e84
Dream-Choi <cmb02236@gmail.com>
2025-08-12 21:06:04 +0900

    fix: Footer의 저작권 표기 수정

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer style={{ textAlign: 'center', padding: '1rem', background: '#eee' }}>
-    © 2025 Pomki. All rights reserved.
+    © 2025 Pomkist. All rights reserved.
     <a href="https://adaptive-licorice-cb4.notion.site/227e13d49b8f805cb67efaa475386d63" target="_blank" rel="noopener noreferrer">
       개인정보처리방침
     </a>
